680f80d92d0b06ee40496bf4af922246cedebbde
Jennifer Oliver <king@us.ibm.com>
2017-01-28 12:16:16 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "MobileClientAccessKituraCredentialsPlugin",
 	dependencies:[
 		.Package(url: "https://github.com/ibm-bluemix-mobile-services/bms-mca-serversdk-swift.git", majorVersion: 0, minor: 5),
-		.Package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", majorVersion: 1, minor: 1)
+		.Package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", majorVersion: 1)
 	]
 )
